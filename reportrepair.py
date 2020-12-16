d0563132ad38b214d19021f60d64887461f606be
Bailey Posante <baileyposante@gmail.com>
2020-12-15 21:28:42 -0500

    adds part 2

--- reportrepair.py
+++ reportrepair.py
@@ -6,8 +6,16 @@ def find2num(list):
             if list[i] + list[j] == 2020:
                 return list[i] * list[j]
                  
+def find3num(list):
+    for i in range(len(list)):
+        for j in range(i + 1, len(list)):
+            for k in range(j + 1, len(list)):
+                if list[i] + list[j] + list[k] == 2020:
+                    return list[i] * list[j] * list[k]
+
 aocinput = open("aoc1input.txt", "r")
 
 list1 = [int(line) for line in aocinput]
 
-print(find2num(list1))
+print(find2num(list1)) 
+print(find3num(list1))
